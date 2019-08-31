1788c48ac16a3eb731af0ba53e7b16fbad3b91c3
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-31 16:49:08 +0530

    Created q5.java

---
+++ q5.java
@@ -0,0 +1,29 @@
+public class q5 {
+ static long hcf(long i,long j)
+ {
+   while(i!=j)
+   {
+     if(i>j)
+     i=i-j;
+     else
+     j=j-i;
+     
+   }return i;
+ }
+ static long lcm(long i,long j)
+ {
+   long lcm;
+   lcm=(i*j)/hcf(i,j);
+   return lcm;
+ }
+
+  public static void main(String[] args) {
+   long j=1;
+   for(int i=11;i<=20;i++)
+   {
+     j=lcm(j,i);
+   }
+   System.out.println(j);
+  }
+
+}
