6801517a351cbdf21bceccd014a3ab97a2065ad2
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-08-31 10:09:05 +0530

    Updated q2.java

---
+++ q2.java
@@ -0,0 +1,16 @@
+public class q2 {
+
+  public static void main(String[] args) {
+    long firstno = 0, secno = 1, result = 2;
+    long sum = 0;
+    while (secno > 4000000) {
+      result = firstno + secno;
+      firstno = secno;
+      secno = result;
+      if (secno % 2 == 0) {
+        sum = sum + secno;
+      }
+    }
+    System.out.println(sum);
+  }
+}
