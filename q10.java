67200301c41f84b1f68b98347cbd2132c738baf1
SouravBear8850 <53810519+SouravBear8850@users.noreply.github.com>
2019-09-01 01:49:24 +0530

    Update q10.java

--- q10.java
+++ q10.java
@@ -1,7 +1,7 @@
 public class q10 {
 public static void main(String[] args){
 		long sum =2;
-		for(int i=3;i<2000000;i++){
+		for(int i=3;i<2000000;i=i +2){
 			if(prime(i)==1)
 			{
 				sum=sum+i;
